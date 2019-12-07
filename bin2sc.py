93675f568560431fddca299a2d7117a0c329676e
SkyBulk <43649816+SkyBulk@users.noreply.github.com>
2019-12-06 21:02:33 -0700

    x32 , x64 shellcode compilation
    TODO: remove unusual constructors from the linker

--- bin2sc.py
+++ bin2sc.py
@@ -17,6 +17,7 @@ import re
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 def sc_bin(obj, syntax):
 	lines = subprocess.check_output(['objdump', '-d', '-M', syntax, obj])
 	lines = lines.split(b'Disassembly of section')[1]
@@ -60,45 +61,62 @@ def sc_arr(obj, syntax):
 			opcode.append("0x"+byte.decode("utf-8"))
 	return shellcode,opcode
 
-def x86_x86_64(arch,payload,obj):
+def x86_x86_64(arch,payload,obj,target, lib=''):
 	if sys.argv[4] == "x86":
-		if os.name == 'nt':
-			pass # Windows
-		elif os.name == "posix":
+		if target == 'win':
+			subprocess.call(['/usr/bin/nasm', '-f', 'win32', payload])
+			subprocess.call(['/usr/bin/ld', '-m', 'i386pe', '-o' , 'shellcode.exe', os.path.splitext(obj)[0]+'.obj'])
+		elif target == "linux":
 			subprocess.call(['/usr/bin/nasm', '-f', 'elf32', payload])
-			subprocess.call(['/usr/bin/ld', '-m', 'elf_i386', '-o' , 'shellcode', os.path.splitext(obj)[0]+'.o'])
+			ld = subprocess.call(['/usr/bin/ld', '-m', 'elf_i386', '-o' , 'shellcode', os.path.splitext(obj)[0]+'.o'])
+			print(ld)
 		else:
 			print('OS not supported')
 	elif sys.argv[4] == "x64":
-		if os.name == 'nt':
-			pass # Windows
-		elif os.name == "posix":
+		if target == 'win':
+			subprocess.call(['/usr/bin/nasm', '-f', 'win64', payload])
+			subprocess.call(['/usr/bin/ld', '-m', 'i386pep', '-o' , 'shellcode.exe', os.path.splitext(obj)[0]+'.obj', lib])
+		elif target == "linux":
 			subprocess.call(['/usr/bin/nasm', '-f', 'elf64', payload])
 			subprocess.call(['/usr/bin/ld', '-m', 'elf_x86_64','-o' , 'shellcode', os.path.splitext(obj)[0]+'.o'])
 		else:
 			print('OS not supported')
 	else:
-		print("arch not found")
+		print("arch not supported")
 
 if __name__ == '__main__':
-	if os.name == 'nt':
-		pass # Windows
-	elif os.name == 'posix':
-		x86_x86_64(sys.argv[4],sys.argv[1],sys.argv[1])
-		if sys.argv[2] == "sc_app" and sys.argv[3] == "linux":
-			opcode = sc_bin('shellcode', 'intel')
-			shellcode = re.sub("(.{32})", "\\1\n",opcode, 0, re.DOTALL)
-			print("[+] block Shellcode")
-			print(shellcode)
-			print("\n[+] linear shellcode\n")
-			print(opcode)
-		elif sys.argv[2] == 'sc_arr' and sys.argv[3] == "linux":
-			opcode = sc_arr('shellcode', 'intel')
-			for sublist in opcode:
-				shellcode = sublist
-			print("\n[+] linear shellcode\n")
-			print('shellcode = {}'.format(str(shellcode).replace("'", '')))
-		else:
-			print("not shellcode format found")
+	try:
+	  lib = sys.argv[5]
+	except IndexError:
+	  lib = None
+
+	if lib == None:
+	    x86_x86_64(sys.argv[4],sys.argv[1],sys.argv[1],sys.argv[3])
 	else:
-		pass
+	    x86_x86_64(sys.argv[4],sys.argv[1],sys.argv[1],sys.argv[3], sys.argv[5])
+
+	if sys.argv[2] == "sc_app":
+		if sys.argv[3] == "linux":
+			opcode = sc_bin('shellcode', 'intel')
+		elif sys.argv[3] == "win":
+			opcode = sc_bin('shellcode.exe', 'intel')
+		else:
+			pass
+		shellcode = re.sub("(.{32})", "\\1\n",opcode, 0, re.DOTALL)
+		print("[+] block Shellcode")
+		print(shellcode)
+		print("\n[+] linear shellcode\n")
+		print(opcode)
+	elif sys.argv[2] == 'sc_arr':
+		if sys.argv[3] == "linux":
+			opcode = sc_arr('shellcode', 'intel')
+		elif sys.argv[3] == "win":
+			opcode = sc_arr('shellcode.exe', 'intel')
+		else:
+			pass
+		for sublist in opcode:
+			shellcode = sublist
+		print("\n[+] linear shellcode\n")
+		print('shellcode = {}'.format(str(shellcode).replace("'", '')))
+	else:
+		print("not shellcode format found")
